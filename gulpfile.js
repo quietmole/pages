09c6aa7daf50c2642c2c62a2c46b58c7742fa6f2
sanemat <o.gata.ken@gmail.com>
2015-10-24 17:03:08 +0900

    refactor(dep): use bookmarkletter instead of bookmarkleter

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var htmlReplace = require('gulp-html-replace');
 var fs = require('fs');
-var bookmarkleter = require('bookmarkleter');
+var bookmarkletter = require('bookmarkletter').bookmarkletter;
 var targets = [
   'create-link-plain',
   'create-link-markdown',
@@ -11,8 +11,8 @@ var targets = [
 gulp.task('build', function() {
   var options = {};
   targets.forEach(function(target) {
-    var compiled = bookmarkleter(fs.readFileSync(target + '.js'), {mangleVars: true, anonymize: true});
-    var forCopy = bookmarkleter('window.prompt("built javascript", "' + compiled + '")', {mangleVars: true, anonymize: true});
+    var compiled = bookmarkletter(fs.readFileSync(target + '.js'));
+    var forCopy = bookmarkletter('window.prompt("built javascript", "' + compiled + '")');
     options[target] = {
       src: [[compiled, forCopy]],
       tpl: '<a href="%s">' + target.replace(/-/g, ' ') +
